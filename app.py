e360ac2919478ecd36323fe1d5444629452d1b4a
Eugene Mefyod <96493615+heavenyoung1@users.noreply.github.com>
2022-10-09 11:49:44 +0300

    update

--- app.py
+++ app.py
@@ -16,10 +16,12 @@ def get_from_env(key):
     dotenv_path = join(dirname(__file__), '.env')
     load_dotenv(dotenv_path)
     return os.environ.get(key)  # возвращен серкетный токен (или ключ к платежной системе)
+
+
 def send_message(chat_id, text):
     method = 'sendMessage'
     token = get_from_env('TOKEN_BOT')
-    url = f'https://api.telegram/bot{token}/{method}'
+    url = f'https://api.telegram.org/bot{token}/{method}'
     data = {'chat_id': chat_id, 'text': text}
     requests.post(url, data=data)
 
